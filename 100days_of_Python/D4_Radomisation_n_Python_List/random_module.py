cd617c8d1ec84b12ad4f4ac9ab4dcaf50e077630
lhtn-bwbw <lhtn.bwbw@gmail.com>
2025-03-03 23:05:40 +0700

    day 4

---
+++ 100days_of_Python/D4_Radomisation_n_Python_List/random_module.py
@@ -0,0 +1,11 @@
+# Day 4: Randomisation and Python lists
+
+import random
+# random_integer = random.randint(1, 100)
+# print('random interger : ' + str(random_integer))
+#  ---
+random_heads_or_tails = random.randint(a=0, b=1)
+if random_heads_or_tails == 0:
+    print('Heads')
+else:
+    print('Tails')
--- 100days_of_Python/Day_4/random_module.py
+++
@@ -1,8 +0,0 @@
-# Day 4: Randomisation and Python lists
-
-import random
-
-random_integer = random.randint(1, 100)
-print('random interger : ' + str(random_integer))
-#  ---
-# my_favourite_number =  s 
